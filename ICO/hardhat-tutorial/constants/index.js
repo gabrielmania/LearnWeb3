fe1c8026a38ba8248f334b52d0fd1c51dc5eb56d
Gabriel Mania <mania.gabb@gmail.com>
2023-02-08 11:57:33 +0800

    added ICO

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xA72bC745210FaB01a16f5BB12C3cA917651bAa58";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
